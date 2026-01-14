df792399f00be5e0167745e777e611fed0145d3a
AtiusAmy <torv.device027@passmail.net>
2026-01-15 06:37:26 +0700

    aaaa

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,52 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+systemctl enable systemd-timesyncd
+systemctl enable systemd-resolved.service
+
+dnf -y install 'dnf5-command(config-manager)'
+
+dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf config-manager setopt tailscale-stable.enabled=0
+dnf -y install --enablerepo='tailscale-stable' tailscale
+
+systemctl enable tailscaled
+
+dnf config-manager addrepo --from-repofile=https://repo.secureblue.dev/secureblue.repo
+dnf config-manager setopt secureblue.enabled=0
+dnf -y install --enablerepo='secureblue' trivalent
+
+# Codecs for video thumbnails on nautilus
+dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-multimedia.repo
+dnf config-manager setopt fedora-multimedia.enabled=0
+dnf -y install --enablerepo=fedora-multimedia \
+    -x PackageKit* \
+    ffmpeg libavcodec @multimedia gstreamer1-plugins-{bad-free,bad-free-libs,good,base} lame{,-libs} libjxl ffmpegthumbnailer
+
+dnf5 copr enable -y secureblue/trivalent
+dnf5 copr enable -y secureblue/run0edit
+
+dnf5 install -y trivalent-subresource-filter adw-gtk3-theme run0edit distrobox
+
+dnf -y copr enable ublue-os/packages
+dnf -y copr disable ublue-os/packages
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:packages install uupd ublue-os-udev-rules
+
+# ts so annoying :face_holding_back_tears: :v: 67
+sed -i 's|uupd|& --disable-module-distrobox|' /usr/lib/systemd/system/uupd.service
+
+systemctl enable uupd.timer
+
+if [ "$(rpm -E "%{fedora}")" == 43 ] ; then
+  dnf -y copr enable ublue-os/flatpak-test
+  dnf -y copr disable ublue-os/flatpak-test
+  dnf -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak flatpak
+  dnf -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-libs flatpak-libs
+  dnf -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-session-helper flatpak-session-helper
+  rpm -q flatpak --qf "%{NAME} %{VENDOR}\n" | grep ublue-os
+fi
+
+
 
 # Use a COPR Example:
 #
@@ -22,3 +67,6 @@ dnf5 install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable bootc-fetch-apply-updates
+systemctl enable flatpak-preinstall.service
+systemctl enable --global bazaar.service
