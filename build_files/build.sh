e930723cccc3cc73b8b300ac380ecf48c182570e
AtiusAmy <torv.device027@passmail.net>
2026-01-16 04:27:21 +0700

    remove

--- build_files/build.sh
+++ build_files/build.sh
@@ -77,3 +77,10 @@ systemctl enable --global bazaar.service
 # Add Flathub to the image for eventual application
 mkdir -p /etc/flatpak/remotes.d/
 curl --retry 3 -Lo /etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub.org/repo/flathub.flatpakrepo
+
+
+
+
+# GO AWAY fedora flatpaks.
+rm -rf /usr/lib/systemd/system/flatpak-add-fedora-repos.service
+systemctl enable flatpak-add-flathub-repos.service
